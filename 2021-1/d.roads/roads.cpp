a01b6b04b47630eae7f1999e270268d4b702cd52
Maksym Skulysh <makskulysh@gmail.com>
2022-03-05 18:18:24 +0200

    roads: passes 2,3,4

--- 2021-1/d.roads/roads.cpp
+++ 2021-1/d.roads/roads.cpp
@@ -1,35 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long mdistx(int n, int x, vector<int> &nroads, vector<vector<vector<int>>> &roads, vector<long long> &tails, vector<int> &cycle)
+long long mdistx(int n, int x, vector<long long> &tails, vector<int> &cycl, int ncy, vector<int> &croads)
 {
-    long long mx = 0, z;
-    long long y;
+//    int y;
     vector<long long> dist(n, -1);
-    dist[x] = 0;
-    vector<int> q(n, -1);
-    y = 1;
-    q[0] = x;
-    while (y!=0) {
-        z = q[y-1];
-        y--;
-        for (int i=0; i<nroads[z]; i++) {
-            if (((dist[roads[z][i][0]] == -1) || (dist[roads[z][i][0]] > dist[z]+roads[z][i][1])) && cycle[roads[z][i][0]] == 1) {
-                q[y] = roads[z][i][0];
-                y++;
-                dist[roads[z][i][0]] = dist[z]+roads[z][i][1];
-            }  
-        }
-    }
-    for (int i=0; i<n; i++) {
+    dist[cycl[x]] = 0;
+    int z = 1;
+    while (dist[cycl[(x+z)%ncy]] == -1 || dist[cycl[(x+z)%ncy]] > dist[cycl[(x+z-1+ncy)%ncy]] + croads[(x+z)%ncy]) {
+        dist[cycl[(x+z)%ncy]] = dist[cycl[(x+z-1+ncy)%ncy]] + croads[(x+z-1)%ncy];
+        z++;
+//        cout<<x<<" 1:";
+//        for (int i=0;i<ncy;i++) {
+//            cout<<dist[cycl[i]]<<' ';
+//        }
+//        cout<<endl;
+    } 
+    z = 1;
+    while (dist[cycl[(x-z+ncy)%ncy]] == -1 || dist[cycl[(x-z+ncy)%ncy]] > dist[cycl[(x-z+1+ncy)%ncy]] + croads[(x-z+ncy)%ncy]) {
+        dist[cycl[(x-z+ncy)%ncy]] = dist[cycl[(x-z+1+ncy)%ncy]] + croads[(x-z+ncy)%ncy];
+        z++;
+//        cout<<x<<" 2:";
+//        for (int i=0;i<ncy;i++) {
+//            cout<<dist[cycl[i]]<<' ';
+//        }
+//        cout<<endl;
+    } 
+    long long mx = 0;
+    for (int i=0; i<ncy; i++) {
 //        cout<<dist[i]<<' ';
-        if (dist[i] + tails[i] > mx &&  dist[i]> 0) {
-            mx = dist[i] + tails[i];
+        if (i != x && dist[cycl[i]] + tails[cycl[i]] > mx) {
+            mx = dist[cycl[i]] + tails[cycl[i]];
         }
     }
 //    cout<<endl;
-    return mx+tails[x];
+    return mx+tails[cycl[x]];
 }
+
+
 
 int main() {
     ios_base::sync_with_stdio(0);
@@ -55,6 +63,7 @@ int main() {
     vector<int> nroads1(nroads);
 
     long long ans = 0;
+    int ncy = n;
 
     for (int i=0; i<n; i++) {
         if (nroads1[i] == 1) {
@@ -62,6 +71,7 @@ int main() {
             long long t = 0;
             while (nroads1[x] < 3) {
                 cycle[x] = 0;
+                ncy--;
 //                cout<<'x'<<x<<endl;
                 for (int j=0; j<nroads[x]; j++) {
 //                    cout<<'r'<<roads[x][j][0]<<endl;
@@ -88,13 +98,44 @@ int main() {
     }
 
 
-    long long m;
+    vector<int> cycl(ncy), croads(ncy);
     for (int i=0;i<n;i++) {
-        if (cycle[i] == 1){
-            m = mdistx(n, i, nroads, roads, tails, cycle);
-            if (m>ans) {
-                ans = m;
+        if (cycle[i] == 1) {
+            cycl[0] = i;
+            bool b = true;
+            for (int j=0;j<nroads[i];j++) {
+                if (cycle[roads[i][j][0]] == 1) {
+                    if (b) {
+                        cycl[1] = roads[i][j][0];
+                        croads[0] = roads[i][j][1];
+                        b = false;
+                    } else {
+                        croads[ncy-1] = roads[i][j][1];
+                    }
+                }
             }
+            break;
+        }
+    }
+//    cout<<ncy<<endl;
+    int y = 2, x;
+    while (y != ncy) {
+        x = cycl[y-1];
+        for (int i=0;i<nroads[x];i++) {
+            if (cycle[roads[x][i][0]] == 1 && roads[x][i][0] != cycl[y-2]) {
+                cycl[y] = roads[x][i][0];
+                croads[y-1] = roads[x][i][1];
+                y++;
+//                cout<<y<<endl;
+                break;
+            }
+        }
+    }
+    long long m;
+    for (int i=0;i<ncy;i++) {
+        m = mdistx(n, i, tails, cycl, ncy, croads);
+        if (m>ans) {
+            ans = m;
         }
     }
     for (int i=0;i<n;i++) {
