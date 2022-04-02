4f22605e167b32c21985c8de5a5a2d60edff7d41
Maksym Skulysh <makskulysh@gmail.com>
2022-04-02 21:27:01 +0300

    2021-2/graph: passes test suites 0-5

--- 2021-2/graph/graph.cpp
+++ 2021-2/graph/graph.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
 
+using namespace __gnu_pbds;
 using namespace std;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+
+struct Ver
+{
+	int p;
+	vector<int> ch;
+};
 
 static void swap(int &a, int &b)
 {
@@ -9,6 +20,19 @@ static void swap(int &a, int &b)
 	b = t;
 }
 
+static void build_tree(int q, vector<vector<int>> &zap, vector<Ver> &tree)
+{
+	tree[0].p = 0;
+	for(int i=1;i<q;i++) {
+		if (zap[i][0] == 3) {
+			tree[i].p = zap[i][1];
+			tree[zap[i][1]].ch.push_back(i);
+		} else {
+			tree[i].p = i-1;
+			tree[i-1].ch.push_back(i);
+		}
+	}
+}
 static int find_root(int x, vector<int> &seg, vector<int> &pseg)
 {
 	int s1 = seg[x];
@@ -24,51 +48,34 @@ static int find_root(int x, vector<int> &seg, vector<int> &pseg)
 	return s1;
 }
 
-void union_lists(int x, int y, vector<int> &seg, vector<list<int>> &segm)
-{
-	list<int> l;
-	int s1 = seg[x], s2 = seg[y];
-	if (s1 != s2) { 
-		auto itr1 = segm[s1].begin(), itr2 = segm[s2].begin();
-		while (itr1 != segm[s1].end() || itr2 != segm[s2].end()) {
-			if (itr1 != segm[s1].end()) {
-				if (itr2 != segm[s2].end()) {
-					if ((*itr1) < (*itr2)) {
-						l.push_back((*itr1));
-						++itr1;
-					} else {
-						l.push_back((*itr2));
-						seg[(*itr2)] = s1;
-						++itr2;
-					}
-				} else {
-					l.push_back((*itr1));
-					++itr1;
-				}
-			} else {
-				l.push_back((*itr2));
-				seg[(*itr2)] = s1;
-				++itr2;
-			}
-		}
-		segm[s1] = l;
-		segm[s2].clear();
-	}
-}
-
-static void zap_3(int x, int n, vector<vector<int>> &zap, vector<int> &seg, vector<list<int>> &segm)
+static void zap_3(int x, int n, int q, vector<vector<int>> &zap, vector<int> &seg, vector<ordered_set> &segm)
 {
 	for (int i=0;i<n;++i) {
 		segm[i].clear();
-		segm[i].push_back(i);
+		segm[i].insert(i);
 		seg[i] = i;
 	}
 	for (int i=0;i<=x;++i) {
 		if (zap[i][0] == 2) {
-			union_lists(zap[i][1], zap[i][2], seg, segm);
+			int s1 = seg[zap[i][1]], s2 = seg[zap[i][2]];
+			if (s1 != s2) {
+				if (segm[s1].size() > segm[s2].size()) {
+					for(auto j=segm[s2].begin();j!=segm[s2].end();++j) {
+						segm[s1].insert((*j));
+						seg[(*j)] = s1;
+					}
+					segm[s2].clear();
+				} else {
+					for(auto j=segm[s1].begin();j!=segm[s1].end();++j) {
+						segm[s2].insert((*j));
+						seg[(*j)] = s2;
+					}
+					segm[s1].clear();
+				}
+			}
 		}
 		if (zap[i][0] == 3 && zap[i][1] != i-1) {
-			zap_3(zap[i][1], n, zap, seg, segm);
+			zap_3(zap[i][1], n, q, zap, seg, segm);
 		}
 	}
 }
@@ -114,31 +121,42 @@ int main()
 			}
 		}
 	} else {
-		vector<list<int>> segm(n);
+		vector<ordered_set> segm(n);
 		vector<int> seg(n);
 		for (int i=0;i<n;++i) {
-			segm[i].push_back(i);
+			segm[i].insert(i);
 			seg[i] = i;
 		}
 		for (int i=0;i<q;++i) {
 			if (zap[i][0] == 1) {
 				int s1 = seg[zap[i][1]];
-				auto itr = segm[s1].begin();
-				for(int j=0;j<zap[i][2] && itr!=segm[s1].end();++j) {
-					++itr;
-				}
-				if (itr!=segm[s1].end()) {
-					cout<<(*itr)+1<<endl;
+				if (segm[s1].size() > zap[i][2]) {
+					cout<<(*segm[s1].find_by_order(zap[i][2]))+1<<endl;
 				} else {
 					cout<<-1<<endl;
 				}
 			}
 			if (zap[i][0] == 2) {
-				union_lists(zap[i][1], zap[i][2], seg, segm);
+				int s1 = seg[zap[i][1]], s2 = seg[zap[i][2]];
+				if (s1 != s2) {
+					if (segm[s1].size() > segm[s2].size()) {
+						for(auto j=segm[s2].begin();j!=segm[s2].end();++j) {
+							segm[s1].insert((*j));
+							seg[(*j)] = s1;
+						}
+						segm[s2].clear();
+					} else {
+						for(auto j=segm[s1].begin();j!=segm[s1].end();++j) {
+							segm[s2].insert((*j));
+							seg[(*j)] = s2;
+						}
+						segm[s1].clear();
+					}
+				}
 			}
 			if (zap[i][0] == 3 && zap[i][1] != i-1) {
-				zap_3(zap[i][1], n, zap, seg, segm);
+				zap_3(zap[i][1], n, q, zap, seg, segm);
 			}
 		}
 	}
-}
+} 
