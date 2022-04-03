61e227cbc005beda3d19567622d01d370d89f0d3
Maksym Skulysh <makskulysh@gmail.com>
2022-04-03 13:17:14 +0300

    2021-2/graph: implemented tree of requests

--- 2021-2/graph/graph.cpp
+++ 2021-2/graph/graph.cpp
@@ -7,11 +7,48 @@ using namespace std;
 typedef tree<int, null_type, less<int>, rb_tree_tag,
 tree_order_statistics_node_update> ordered_set;
 
+struct Node
+{
+	int idx;
+	int dep;
+};
+
 struct Ver
 {
 	int p;
-	vector<int> ch;
+	list<Node> ch;
 };
+
+static void build_tree(int q, vector<vector<int>> &zap, vector<Ver> &tree)
+{
+	tree[0].p = 0;
+	vector<int> dep(q+1, 0);
+	for(int i=1;i<q+1;i++) {
+		Node nd;
+		nd.idx = i;
+		nd.dep = 0;
+		if (zap[i][0] == 3) {
+			tree[i].p = zap[i][1];
+			tree[zap[i][1]].ch.push_back(nd);
+		} else {
+			tree[i].p = i-1;
+			tree[i-1].ch.push_back(nd);
+		}
+	}
+	for(int i=q;i>0;i--) {
+		if (dep[i]+1 > dep[tree[i].p]) {
+			dep[tree[i].p] = dep[i]+1;
+		}
+	}
+	for(int i=0;i<q;++i) {
+		for(auto j=tree[i].ch.begin();j!=tree[i].ch.end();++j) {
+			(*j).dep = dep[(*j).idx];
+		}
+		tree[i].ch.sort([](Node &a, Node &b) {
+	        return a.dep < b.dep || (a.dep == b.dep && a.idx < b.idx);
+	    });
+	}
+}
 
 static void swap(int &a, int &b)
 {
@@ -20,63 +57,29 @@ static void swap(int &a, int &b)
 	b = t;
 }
 
-static void build_tree(int q, vector<vector<int>> &zap, vector<Ver> &tree)
-{
-	tree[0].p = 0;
-	for(int i=1;i<q;i++) {
-		if (zap[i][0] == 3) {
-			tree[i].p = zap[i][1];
-			tree[zap[i][1]].ch.push_back(i);
-		} else {
-			tree[i].p = i-1;
-			tree[i-1].ch.push_back(i);
-		}
-	}
-}
-static int find_root(int x, vector<int> &seg, vector<int> &pseg)
+static int zap_1(int x, int k, vector<int> &seg, vector<ordered_set> &segm)
 {
 	int s1 = seg[x];
-	while(s1 != pseg[s1]) {
-		s1 = pseg[s1];
+	if (segm[s1].size() > k) {
+		return (*segm[s1].find_by_order(k))+1;
+	} else {
+		return -1;
 	}
-	int s2 = seg[x], t;
-	while (s2 != s1) {
-		t = pseg[s2];
-		pseg[s2] = s1;
-		s2 = t;
-	}
-	return s1;
 }
 
-static void zap_3(int x, int n, int q, vector<vector<int>> &zap, vector<int> &seg, vector<ordered_set> &segm)
+
+static void marge(int x, int y, vector<int> &seg, vector<ordered_set> &segm)
 {
-	for (int i=0;i<n;++i) {
-		segm[i].clear();
-		segm[i].insert(i);
-		seg[i] = i;
-	}
-	for (int i=0;i<=x;++i) {
-		if (zap[i][0] == 2) {
-			int s1 = seg[zap[i][1]], s2 = seg[zap[i][2]];
-			if (s1 != s2) {
-				if (segm[s1].size() > segm[s2].size()) {
-					for(auto j=segm[s2].begin();j!=segm[s2].end();++j) {
-						segm[s1].insert((*j));
-						seg[(*j)] = s1;
-					}
-					segm[s2].clear();
-				} else {
-					for(auto j=segm[s1].begin();j!=segm[s1].end();++j) {
-						segm[s2].insert((*j));
-						seg[(*j)] = s2;
-					}
-					segm[s1].clear();
-				}
-			}
+	int s1 = seg[x], s2 = seg[y];
+	if (s1 != s2) {
+		if (segm[s1].size() < segm[s2].size()) {
+			swap(s1, s2);
 		}
-		if (zap[i][0] == 3 && zap[i][1] != i-1) {
-			zap_3(zap[i][1], n, q, zap, seg, segm);
+		for(auto j=segm[s2].begin();j!=segm[s2].end();++j) {
+			segm[s1].insert((*j));
+			seg[(*j)] = s1;
 		}
+		segm[s2].clear();
 	}
 }
 
@@ -84,79 +87,64 @@ int main()
 {
 	int n, q, g;
 	cin>>n>>q>>g;
-	vector<vector<int>> zap(q, vector<int>(3));
-	for (int i=0;i<q;++i) {
+	vector<vector<int>> zap(q+1, vector<int>(3));
+	for (int i=1;i<=q;++i) {
 		cin>>zap[i][0]>>zap[i][1];
-		zap[i][1]--;
 		if (zap[i][0] != 3) {
 			cin>>zap[i][2];
+			zap[i][1]--;
 			zap[i][2]--;
 		}
 	}
-	if (g == 4) {
-		vector<int> seg(n), dep(n), pseg(n);
-		for (int i=0;i<n;++i) {
-			seg[i] = i;
-			pseg[i] = i;
-			dep[i] = 1;
-		}
-		for (int i=0;i<q;++i) {
-			if (zap[i][0] == 1) {
-				int s1 = find_root(zap[i][1], seg, pseg);
-				if (dep[s1] > zap[i][2]) {
-					cout<<s1 + zap[i][2]+1<<endl;
-				} else {
-					cout<<-1<<endl;
-				}
-			}
-			if (zap[i][0] == 2) {
-				int s1 = find_root(zap[i][1], seg, pseg), s2 = find_root(zap[i][2], seg, pseg);
-				if (s1 != s2) {
-					if (s2 < s1) {
-						swap(s1, s2);
-					}
-					pseg[s2] = s1;
-					dep[s1] += dep[s2];
-				}
+
+	vector<Ver> tree(q+1);
+	build_tree(q, zap, tree);
+
+	vector<ordered_set> segm(n);
+	vector<int> seg(n);
+	for (int i=0;i<n;++i) {
+		segm[i].insert(i);
+		seg[i] = i;
+	}
+
+	vector<vector<int>> m_seg(q+1);
+	vector<vector<ordered_set>> m_segm(q+1);
+
+	vector<int> res(q, -2);
+	int x = 0;
+	while (!tree[0].ch.empty()){
+		if (!(m_seg[x].empty() && m_segm[x].empty())) {
+			seg = m_seg[x];
+			segm = m_segm[x];
+			if (tree[x].ch.begin()++ == tree[x].ch.end()) {
+				m_seg[x].clear();
+				m_segm[x].clear();
 			}
 		}
-	} else {
-		vector<ordered_set> segm(n);
-		vector<int> seg(n);
-		for (int i=0;i<n;++i) {
-			segm[i].insert(i);
-			seg[i] = i;
-		}
-		for (int i=0;i<q;++i) {
-			if (zap[i][0] == 1) {
-				int s1 = seg[zap[i][1]];
-				if (segm[s1].size() > zap[i][2]) {
-					cout<<(*segm[s1].find_by_order(zap[i][2]))+1<<endl;
-				} else {
-					cout<<-1<<endl;
-				}
+		if (x != 0) {
+			if (res[x-1] == -2 && zap[x][0] == 1) {
+				res[x-1] = zap_1(zap[x][1], zap[x][2], seg, segm);
 			}
-			if (zap[i][0] == 2) {
-				int s1 = seg[zap[i][1]], s2 = seg[zap[i][2]];
-				if (s1 != s2) {
-					if (segm[s1].size() > segm[s2].size()) {
-						for(auto j=segm[s2].begin();j!=segm[s2].end();++j) {
-							segm[s1].insert((*j));
-							seg[(*j)] = s1;
-						}
-						segm[s2].clear();
-					} else {
-						for(auto j=segm[s1].begin();j!=segm[s1].end();++j) {
-							segm[s2].insert((*j));
-							seg[(*j)] = s2;
-						}
-						segm[s1].clear();
-					}
-				}
-			}
-			if (zap[i][0] == 3 && zap[i][1] != i-1) {
-				zap_3(zap[i][1], n, q, zap, seg, segm);
+			if (res[x-1] == -2 && zap[x][0] == 2) {
+				marge(zap[x][1], zap[x][2], seg, segm);
+				res[x-1] = -3;
 			}
 		}
+		if (g != 4 && g!=5 && !tree[x].ch.empty() && tree[x].ch.begin()++ != tree[x].ch.end()) {
+			m_seg[x] = seg;
+			m_segm[x] = segm;
+		}
+		int p = tree[x].p;
+		if (tree[x].ch.empty()) {
+			tree[p].ch.pop_front();
+			x = p;
+		} else {
+			x = tree[x].ch.front().idx;
+		}
+	}
+	for (int i=0;i<q;i++) {
+		if (res[i] > -2) {
+			cout<<res[i]<<endl;
+		} 
 	}
 } 
