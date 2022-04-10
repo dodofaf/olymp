91b4914e14b337415f2555cc194f5a6938bdd2d7
Maksym Skulysh <makskulysh@gmail.com>
2022-04-10 20:42:55 +0300

    2021-2/graph: passes test suites 0-9

--- 2021-2/graph/graph.cpp
+++ 2021-2/graph/graph.cpp
@@ -7,46 +7,25 @@ using namespace std;
 typedef tree<int, null_type, less<int>, rb_tree_tag,
 tree_order_statistics_node_update> ordered_set;
 
-struct Node
-{
-	int idx;
-	int dep;
-};
+
 
 struct Ver
 {
 	int p;
-	list<Node> ch;
+	list<int> ch;
 };
 
 static void build_tree(int q, vector<vector<int>> &zap, vector<Ver> &tree)
 {
 	tree[0].p = 0;
-	vector<int> dep(q+1, 0);
 	for(int i=1;i<q+1;i++) {
-		Node nd;
-		nd.idx = i;
-		nd.dep = 0;
 		if (zap[i][0] == 3) {
 			tree[i].p = zap[i][1];
-			tree[zap[i][1]].ch.push_back(nd);
+			tree[zap[i][1]].ch.push_back(i);
 		} else {
 			tree[i].p = i-1;
-			tree[i-1].ch.push_back(nd);
+			tree[i-1].ch.push_back(i);
 		}
-	}
-	for(int i=q;i>0;i--) {
-		if (dep[i]+1 > dep[tree[i].p]) {
-			dep[tree[i].p] = dep[i]+1;
-		}
-	}
-	for(int i=0;i<q;++i) {
-		for(auto j=tree[i].ch.begin();j!=tree[i].ch.end();++j) {
-			(*j).dep = dep[(*j).idx];
-		}
-		tree[i].ch.sort([](Node &a, Node &b) {
-	        return a.dep < b.dep || (a.dep == b.dep && a.idx < b.idx);
-	    });
 	}
 }
 
@@ -57,27 +36,27 @@ static void swap(int &a, int &b)
 	b = t;
 }
 
-static int find_root(int x, vector<int> &seg, vector<int> &pseg)
+static int find_root(int x, vector<int> &pseg)
 {
-	int s1 = seg[x];
+	int s1 = x;
 	while(s1 != pseg[s1]) {
 		s1 = pseg[s1];
 	}
 	return s1;
 }
 
-static int zap_1(int x, int k, int n, int ns, int s, vector<vector<int>> &c, vector<int> &seg, vector<int> &pseg)
+static int zap_1(int x, int k, int n, int ns, int s, vector<vector<short int>> &c, vector<int> &pseg)
 {
 	int d = 0;
-	int s1 = find_root(seg[x], seg, pseg);
-	while (d != ns && k >= c[d][s1]) {
-		k-=c[d][s1];
+	int s1 = find_root(x, pseg);
+	while (d != ns && k >= c[s1][d]) {
+		k-=c[s1][d];
 		d++;
 	}
 	if (d != ns) {
 		int mx = min((d+1)*s, n);
 		for (int i=d*s;i<mx;i++) {
-			int si = find_root(seg[i], seg, pseg);
+			int si = find_root(i, pseg);
 			if (si == s1) {
 				k--;
 			}
@@ -90,15 +69,26 @@ static int zap_1(int x, int k, int n, int ns, int s, vector<vector<int>> &c, vec
 }
 
 
-static int marge(int x, int y, int z, int ns, vector<vector<int>> &c, vector<int> &seg, vector<int> &pseg, vector<int> &res)
+static int marge(int x, int y, int z, int ns, vector<vector<short int>> &c, vector<int> &pseg, vector<int> &rank,
+ vector<int> &tick, vector<int> &res)
 {
-	int s1 = find_root(seg[x], seg, pseg), s2 = find_root(seg[y], seg, pseg);
+	int s1 = find_root(x, pseg), s2 = find_root(y, pseg);
+	int t = 2;
 	if (s1 != s2) {
+		if (rank[s1]<rank[s2]) {
+			swap(s1, s2);
+			t = 1;
+		}
 		for (int i=0;i<ns;++i) {
-			c[i][s1] += c[i][s2];
+			c[s1][i] += c[s2][i];
 		}
 		pseg[s2] = s1;
 		res[z-1] = -3;
+		tick[z] = t;
+		if (rank[s1] == rank[s2]) {
+			++rank[s1];
+			tick[z] *= -1;
+		}
 		return 2;
 	}
 	return 4;
@@ -122,120 +112,66 @@ int main()
 	}
 	vector<Ver> tree(q+1);
 	build_tree(q, zap, tree);
+	vector<int> pseg(n), rank(n, 1), tick(q+1, 0);
+	for (int i=0;i<n;++i) {
+		pseg[i] = i;
+	}
+	int s = sqrt(n), ns = s;
+	if (s*s != n) {
+		ns++;
+	}
+	if (n>ns*s) {
+		ns++;
+	}
+	vector<vector<short int>> c(n, vector<short int>(ns, 0));
+	for (int i=0;i<n;++i) {
+		c[i][i/s] = 1;
+	}
 
-	if (g == 41 || g == 61) {
-		vector<int> seg(n), dep(n, 1), pseg(n);
-		for (int i=0;i<n;++i) {
-			seg[i] = i;
-			pseg[i] = i;
-		}
-		int x = 0;
-		vector<int> res(q, -2);
-		while (!tree[0].ch.empty()){
-			if (x != 0) {
-				if (res[x-1] == -2 && zap[x][0] == 1) {
-					int s1 = find_root(zap[x][1], seg, pseg);
-					if (dep[s1] > zap[x][2]) {
-						res[x-1] = s1 + zap[x][2]+1;
-					} else {
-						res[x-1] = -1;
+	vector<int> res(q, -2);
+	int x = 0;
+	while (!tree[0].ch.empty()){
+		if (x != 0) {
+			if (res[x-1] == -2 && zap[x][0] == 1) {
+				res[x-1] = zap_1(zap[x][1], zap[x][2], n, ns, s, c, pseg);
+			}
+			if (zap[x][0] == 2) {
+				if (res[x-1] == -2) {
+					if (!tree[x].ch.empty()) {
+						zap[x][0] = marge(zap[x][1], zap[x][2], x, ns, c, pseg, rank, tick, res);
 					}
-				}
-				if (zap[x][0] == 2) {
-					if (res[x-1] ==  -2) {
-						if (!tree[x].ch.empty()) {
-							int s1 = find_root(zap[x][1], seg, pseg), s2 = find_root(zap[x][2], seg, pseg);
-							if (s1 != s2) {
-								pseg[s2] = s1;
-								dep[s1] += dep[s2];
-								res[x-1] = -3;
-							} else {
-								zap[x][0] = 4;
-							}
+				} else {
+					if (tree[x].ch.empty()) {
+						int s2 = zap[x][abs(tick[x])];
+						int s1 = pseg[s2];
+						while(pseg[s1] != s1) {
+							s2 = s1;
+							s1 = pseg[s1];
 						}
-					} else {
-						if(tree[x].ch.empty()){
-							int s1 = zap[x][1], s2 = zap[x][2];
-							s1 = find_root(s1, seg, pseg);
-							dep[s1] -= dep[s2];
-							pseg[s2] = s2;
-							res[x-1] = -2;
+						for (int i=0;i<ns;i++) {
+							c[s1][i] -= c[s2][i];
 						}
- 					}
-				}
-			}
-			if (tree[x].ch.empty()) {
-				x = tree[x].p;
-				tree[x].ch.pop_front();
-			} else {
-				x = tree[x].ch.front().idx;
-			}
-		}
-		for (int i=0;i<q;i++) {
-			if (res[i] != -2) {
-				cout<<res[i]<<endl;
-			} 
-		}
-	} else {
-		vector<int> seg(n), pseg(n);
-		for (int i=0;i<n;++i) {
-			seg[i] = i;
-			pseg[i] = i;
-		}
-		int s = sqrt(n), ns = s;
-		if (s*s != n) {
-			ns++;
-		}
-		if (n>ns*s) {
-			ns++;
-		}
-		vector<vector<int>> c(ns, vector<int>(n, 0));
-		for (int i=0;i<n;++i) {
-			c[i/s][i] = 1;
-		}
-
-		vector<int> res(q, -2);
-		int x = 0;
-		while (!tree[0].ch.empty()){
-			if (x != 0) {
-				if (res[x-1] == -2 && zap[x][0] == 1) {
-					res[x-1] = zap_1(zap[x][1], zap[x][2], n, ns, s, c, seg, pseg);
-				}
-				if (zap[x][0] == 2) {
-					if (res[x-1] == -2) {
-						if (!tree[x].ch.empty()) {
-							zap[x][0] = marge(zap[x][1], zap[x][2], x, ns, c, seg, pseg, res);
+						if (tick[x] < 0) {
+							--rank[s1];
 						}
-					} else {
-						if (tree[x].ch.empty()) {
-							int s2 = seg[zap[x][2]];
-							int s1 = pseg[s2];
-							while(pseg[s1] != s1) {
-								s2 = s1;
-								s1 = pseg[s1];
-							}
-							for (int i=0;i<ns;i++) {
-								c[i][s1] -= c[i][s2];
-							}
-							pseg[s2] = s2;
-						}
+						pseg[s2] = s2;
 					}
 				}
 			}
-			if (tree[x].ch.empty()) {
-				if (g == 4 || g == 5) {
-					break;
-				}
-				x = tree[x].p;
-				tree[x].ch.pop_front();
-			} else {
-				x = tree[x].ch.front().idx;
+		}
+		if (tree[x].ch.empty()) {
+			if (g == 4 || g == 5) {
+				break;
 			}
+			x = tree[x].p;
+			tree[x].ch.pop_front();
+		} else {
+			x = tree[x].ch.front();
 		}
-		for (int i=0;i<q;i++) {
-			if (res[i] > -2) {
-				cout<<res[i]<<endl;
-			} 
-		}
+	}
+	for (int i=0;i<q;i++) {
+		if (res[i] > -2) {
+			cout<<res[i]<<endl;
+		} 
 	}
 } 
