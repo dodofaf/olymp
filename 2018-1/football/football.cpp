720d213fa6717511c481e26845d01688c1531aec
Maksym Skulysh <makskulysh@gmail.com>
2022-07-18 19:00:30 +0200

    olymp/2018-1/football: improved set

--- 2018-1/football/football.cpp
+++ 2018-1/football/football.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <set>
 
 using namespace std;
 
@@ -13,6 +14,12 @@ struct Segment
 	int y1;
 	int x2;
 	int y2;
+	int ind;
+};
+
+auto cmp = [](const Segment &a, const Segment &b)
+{
+    return a.x1 < b.x1 || (a.x1 == b.x1 && a.ind < b.ind);
 };
 
 int main()
@@ -28,17 +35,24 @@ int main()
 	istringstream iss1(line);
 	iss1>>q;
 	vector<Segment> seg(q);
+	set<Segment, decltype(cmp)> sorted_by_x1(cmp);
 	for (int i=0;i<q;i++) {
 		getline(sin, line);
 		istringstream iss(line);
 		iss>>seg[i].x1>>seg[i].y1>>seg[i].x2>>seg[i].y2;
+		seg[i].ind = i;
+		sorted_by_x1.insert(seg[i]);
 	}
 	fstream sout("football.out", sout.out);
 	long long cnt = 0;
 	for (int i=0;i<q;i++) {
 		cnt--;
-		for (int j=0;j<q;j++) {
-			if ((seg[i].x1<=seg[j].x2 && seg[i].x2>=seg[j].x1) && (seg[i].y1<=seg[j].y2 && seg[i].y2>=seg[j].y1)) {
+		Segment s;
+		s.ind = -1;
+		s.x1 = seg[i].x2+1;
+		auto itr_e = sorted_by_x1.insert(s).first;
+		for (auto itr=sorted_by_x1.begin();itr!=itr_e;itr++) {
+			if ((seg[i].x1<=(*itr).x2 && seg[i].x2>=(*itr).x1) && (seg[i].y1<=(*itr).y2 && seg[i].y2>=(*itr).y1)) {
 				cnt++;
 			}
 		}
