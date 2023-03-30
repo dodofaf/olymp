630c34a6f798aae54415c3ac4ae92e8da765f445
Maksym Skulysh <makskulysh@gmail.com>
2023-03-30 12:49:50 +0300

    olymp/2023-1/square: tried to do with __int128_t, but it work too long with big n.

--- 2023-1/square/square.cpp
+++ 2023-1/square/square.cpp
@@ -1,41 +1,24 @@
 #include <iostream>
 #include <iterator>
-#include <list>
 #include <cstdio>
+#include <map>
+#include <cstdint>
 
 #define f first
 #define s second
 
 using namespace std;
 
-void mer(list<pair<int, int>> &l1, list<pair<int, int>> &l2, bool d)
+__int128_t gcd(__int128_t a, __int128_t b)
 {
-    auto itr1 = l1.begin(), itr2 = l2.begin();
-    for (;itr1!=l1.end() && itr2!=l2.end();) {
-        if (itr1->f==itr2->f) {
-            if (d) itr1->s += itr2->s;
-            else itr1->s -= itr2->s;
-            
-            if (!itr1->s) {
-                auto itr = itr1;
-                ++itr1;
-                l1.erase(itr);
-            } else  ++itr1;
-            ++itr2;
-        } else if (itr1->f<itr2->f) ++itr1;
-        else {
-            if (d) l1.insert(itr1, {itr2->f, itr2->s});
-            else l1.insert(itr1, {itr2->f, -(itr2->s)});
-            ++itr2;
-        }
+    while (a!=0 && b!=0) {
+        if (a>=b) a%=b;
+        else b%=a;
     }
-    for (;itr2!=l2.end();++itr2) {
-        if (d) l1.insert(itr1, {itr2->f, itr2->s});
-        else l1.insert(itr1, {itr2->f, -(itr2->s)});
-    }
+    return a+b;
 }
 
-list<pair<int, int>> ar[1500][1500];
+pair<__int128_t, __int128_t> ar[1000][1000];
 
 int main() {
     ios_base::sync_with_stdio(0);
@@ -48,68 +31,82 @@ int main() {
     for (int i=0;i<n;++i) {
         int a;
         cin>>a;
-        if (a!=1) {
-            int cnt = 0;
-            while (!(a&1)) {
-                cnt++;
-                a>>=1;
-            }
-            if (cnt) ar[i][i].push_back({2, cnt});
-            for (int j=3;j*j<=a;j+=2) {
-                cnt = 0;
-                while (a%j==0) {
-                    cnt++;
-                    a/=j;
-                }
-                if (cnt) ar[i][i].push_back({j, cnt});
-            }
-            if (a!=1) ar[i][i].push_back({a, 1});
-        }
+        ar[i][i].f = a;
+        ar[i][i].s=1;
     }
+    
     for (int i=0;i<n-1;++i) {
         int a;
         cin>>a;
-        if (a!=1) {
-            int cnt = 0;
-            while (!(a&1)) {
-                cnt++;
-                a>>=1;
-            }
-            if (cnt) ar[i+1][i].push_back({2, cnt});
-            for (int j=3;j*j<=a;j+=2) {
-                cnt = 0;
-                while (a%j==0) {
-                    cnt++;
-                    a/=j;
-                }
-                if (cnt) ar[i+1][i].push_back({j, cnt});
-            }
-            if (a!=1) ar[i+1][i].push_back({a, 1});
-        }
+        ar[i+1][i].f = a;
+        ar[i+1][i].s=1;
     }
     
     int p, q;
     cin>>p>>q;
+    __int128_t g;
     if (p<q) {
         for (int i=0;i<q-p;++i){
-            for (int x=p-1, y=p+i;x<q;++y, ++x) {
-                ar[x][y] = ar[x][y-1];
-                mer(ar[x][y], ar[x+1][y], true);
-                mer(ar[x][y], ar[x+1][y-1], false);
-                ar[x+1][y-1].clear();
+            for (int x=p-1, y=p+i;y<q;++y, ++x) {
+                ar[x][y].f = ar[x+1][y].f * ar[x][y-1].f * ar[x+1][y-1].s;
+                ar[x][y].s = ar[x+1][y].s * ar[x][y-1].s * ar[x+1][y-1].f;
+                g = gcd(ar[x][y].f, ar[x][y].s);
+                ar[x][y].f/=g;
+                ar[x][y].s/=g;
             }
         }
     }
     else if (p>q+1) {
         for (int i=1;i<p-q;++i){
             for (int x=q+i, y=q-1;x<p;++y, ++x) {
-                ar[x][y] = ar[x][y+1];
-                mer(ar[x][y], ar[x-1][y], true);
-                mer(ar[x][y], ar[x-1][y+1], false);
-                ar[x-1][y+1].clear();
+                ar[x][y].f = ar[x-1][y].f * ar[x][y+1].f * ar[x-1][y+1].s;
+                ar[x][y].s = ar[x-1][y].s * ar[x][y+1].s * ar[x-1][y+1].f;
+                g = gcd(ar[x][y].f, ar[x][y].s);
+                ar[x][y].f/=g;
+                ar[x][y].s/=g;
             }
         }
     }
-    if (ar[p-1][q-1].empty()) cout<<"1 1\n";
-    else for (auto itr=ar[p-1][q-1].begin();itr!=ar[p-1][q-1].end();++itr) cout<<itr->f<<' '<<itr->s<<'\n';
+    
+    if (ar[p-1][q-1].f == 1 && ar[p-1][q-1].s == 1) cout<<"1 1\n";
+    else {
+        map<int, int> m;
+        if (ar[p-1][q-1].f!=1) {
+            int cnt = 0;
+            while (!(ar[p-1][q-1].f&1)) {
+                cnt++;
+                ar[p-1][q-1].f>>=1;
+            }
+            if (cnt) m[2] = cnt;
+            for (long long i=3;i*i<=ar[p-1][q-1].f;i+=2) {
+                cnt = 0;
+                while (ar[p-1][q-1].f%i==0) {
+                    cnt++;
+                    ar[p-1][q-1].f/=i;
+                }
+                if (cnt) m[(int)i] = cnt;
+            }
+            if (ar[p-1][q-1].f!=1) m[(int)ar[p-1][q-1].f] = 1;
+        }
+        
+        if (ar[p-1][q-1].s!=1) {
+            int cnt = 0;
+            while (!(ar[p-1][q-1].s&1)) {
+                cnt++;
+                ar[p-1][q-1].s>>=1;
+            }
+            if (cnt) m[2] = -cnt;
+            for (long long i=3;i*i<=ar[p-1][q-1].s;i+=2) {
+                cnt = 0;
+                while (ar[p-1][q-1].s%i==0) {
+                    cnt++;
+                    ar[p-1][q-1].s/=i;
+                }
+                if (cnt) m[(int)i] = -cnt;
+            }
+            if (ar[p-1][q-1].s!=1) m[(int)ar[p-1][q-1].s] = -1;
+        }
+        
+        for (const auto& [k, v] : m) cout<<k<<' '<<v<<'\n';
+    }
 }
