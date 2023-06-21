7338310faff8498f7057c1f4d14ad917985ae036
Maksym Skulysh <makskulysh@gmail.com>
2023-06-21 13:28:36 +0300

    third version. changed method of finding l2, but program still doesn't pass any of test suits.

--- 2023-2/product/product.cpp
+++ 2023-2/product/product.cpp
@@ -9,7 +9,6 @@ struct Segment
 {
     int l;
     int r;
-    int mn;
     int mn1;
     int mn2;
 };
@@ -41,40 +40,70 @@ void building_seg_tree(int ar[], int n)
 }
 
 int lm[1000005] = {0};
+int lm2[1000005] = {0};
 
 void finding_ind_left_min_for_each_el(int ar[], int n)
 {
-    stack<int> s;
-    s.push(n-1);
+    stack<int> s1, s2, temp;
+    s1.push(n-1);
     for (int i=n-2;i>-1;--i) {
-        while (!s.empty() && ar[i]<ar[s.top()]) {
-            lm[s.top()] = i;
-            s.pop();
+        while (!s2.empty() && (ar[i]<ar[s2.top()] || ar[i]<ar[lm[s2.top()]])) {
+            lm2[s2.top()] = i;
+            s2.pop();
         }
-        s.push(i);
+        while (!s1.empty() && ar[i]<ar[s1.top()]) {
+            lm[s1.top()] = i;
+            temp.push(s1.top());
+            s1.pop();
+        }
+        while (!temp.empty()) {
+            s2.push(temp.top());
+            temp.pop();
+        }
+        s1.push(i);
     }
-    while (!s.empty()) {
-        lm[s.top()] = -1;
-        s.pop();
+    while (!s1.empty()) {
+        lm[s1.top()] = -1;
+        lm2[s1.top()] = -1;
+        s1.pop();
+    }
+    while (!s2.empty()) {
+        lm2[s2.top()] = -1;
+        s2.pop();
     }
 }
 
 int rm[1000005] = {0};
+int rm2[1000005] = {0};
 
 void finding_ind_right_min_for_each_el(int ar[], int n)
 {
-    stack<int> s;
-    s.push(0);
+    stack<int> s1, s2, temp;
+    s1.push(0);
     for (int i=1;i<n;++i) {
-        while (!s.empty() && ar[i]<ar[s.top()]) {
-            rm[s.top()] = i;
-            s.pop();
+        while (!s2.empty() && (ar[i]<ar[s2.top()] || ar[i]<ar[lm[s2.top()]])) {
+            rm2[s2.top()] = i;
+            s2.pop();
         }
-        s.push(i);
+        while (!s1.empty() && ar[i]<ar[s1.top()]) {
+            rm[s1.top()] = i;
+            temp.push(s1.top());
+            s1.pop();
+        }
+        while (!temp.empty()) {
+            s2.push(temp.top());
+            temp.pop();
+        }
+        s1.push(i);
     }
-    while (!s.empty()) {
-        rm[s.top()] = n;
-        s.pop();
+    while (!s1.empty()) {
+        rm[s1.top()] = n;
+        rm2[s1.top()] = n;
+        s1.pop();
+    }
+    while (!s2.empty()) {
+        rm2[s2.top()] = n;
+        s2.pop();
     }
 }
 
@@ -95,49 +124,25 @@ int main() {
     cin>>n;
     for (int i=0;i<n;++i) cin>>ar[i];
     
-    building_seg_tree(ar, n);
+//    building_seg_tree(ar, n);
     
     finding_ind_left_min_for_each_el(ar, n);
     finding_ind_right_min_for_each_el(ar, n);
     
-    int mn1 = min(ar[0], ar[1]), mn2 = max(ar[0], ar[1]);
-    for (int i=2;i<n;++i) {
-        if (ar[i]<mn1) {
-            mn2 = mn1;
-            mn1 = ar[i];
-        } else if (ar[i]<mn2) mn2 = ar[i];
-    }
-    
     int res = 0;
     
-    for (int i=1;i<n;++i) {
-        if (ar[i] == mn1) res = max(res, (mn1+mn2)*n);
-        if (lm[i] == -1) continue;
-        int l = lm[i], r = max(rm[i]-1, i);
-        if (l != 0) {
-            for (int j=1;;) {
-                if (st[(j<<1)|1].l==st[(j<<1)|1].r && st[(j<<1)|1].mn<ar[i]) {
-                    l = min(l, min(st[(j<<1)|1].l+1, i));
-                    break;
-                }
-                if (st[j<<1].l==st[j<<1].r && st[j<<1].mn<ar[i]) {
-                    l = min(l, min(st[j<<1].l+1, i));
-                    break;
-                }
-                if (st[j<<1].l==st[j<<1].r && st[j<<1].l==0) {
-                    l = 0;
-                    break;
-                }
-                
-                if (st[(j<<1)|1].l<l && ((check(l, (j<<1)|1) && st[(j<<1)|1].mn2<ar[i]) || (!check(l, (j<<1)|1) && st[(j<<1)|1].mn1<ar[i]))) {
-                    j<<=1;
-                    ++j;
-                } else {
-                    j<<=1;
-                }
-            }
+    for (int i=0;i<n;++i) {
+        if (lm[i] != -1) {
+            int l = lm2[i], r = max(rm[i]-1, i);
+            res = max(res, (ar[lm[i]]+ar[i])*(r-l));
         }
-        res = max(res, (ar[lm[i]]+ar[i])*(r-l+1));
+        if (rm[i] != n) {
+            int l = min(lm[i]+1, i), r = rm2[i];
+            res = max(res, (ar[rm[i]]+ar[i])*(r-l));
+        }
+        if (lm[i] == -1 && rm[i] == n) {
+            res = max(res, 2*ar[i]*n);
+        }
     }
     cout<<res<<'\n';
 }
