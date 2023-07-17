c23ad76bc04d17846e1e6739e616fd27572a77c3
dodofaf <makskulysh@gmail.com>
2023-07-17 22:05:05 +0300

    passes 1/6 test suit and basic test.

---
+++ 4stage/2023/coins/coins.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int query(int l1, int l2, int a1[], int a2[])
+{
+    cout<<"? "<<l1<<' '<<l2;
+    for (int i=0;i<l1;++i) cout<<' '<<a1[i];
+    for (int i=0;i<l2;++i) cout<<' '<<a2[i];
+    cout<<'\n';
+    cout.flush();
+    int res;
+    cin>>res;
+    return res;
+}
+
+void answer(int ans)
+{
+    cout<<"! "<<ans<<'\n';
+    cout.flush();
+}
+
+int a1[10000] = {0};
+int a2[10000] = {0};
+
+void g1(int n, int k)
+{
+    a1[0] = 1;
+    a2[0] = n;
+    if (query(1, 1, a1, a2) == 2) return answer(1);
+    for (int i=2;i<n;++i) {
+        a2[0] = i;
+        if (query(1, 1, a1, a2) == 1) return answer(i);
+    }
+    answer(n);
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, k, g;
+    cin>>n>>k>>g;
+    if (g <= 1) g1(n, k);
+    if (g>1) answer(1);
+}
